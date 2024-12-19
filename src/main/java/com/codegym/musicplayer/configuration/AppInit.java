0d6f46e39d87ee2f9be154a4623248eb47b8acbd
tmtri3102 <tmtri310251@gmail.com>
2024-12-19 16:34:26 +0700

    add configugration package

--- src/main/java/com/codegym/musicplayer/configuration/AppInit.java
+++ src/main/java/com/codegym/musicplayer/configuration/AppInit.java
@@ -3,9 +3,10 @@ package com.codegym.musicplayer.configuration;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{AppConfiguration.class};
+        return new Class[0];
     }
 
     @Override
