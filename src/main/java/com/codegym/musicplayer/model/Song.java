7b680d972cc5538a47fc44bb469b7bc18b8e7584
tmtri3102 <tmtri310251@gmail.com>
2024-12-19 16:44:43 +0700

    add 2 models

--- src/main/java/com/codegym/musicplayer/model/Song.java
+++ src/main/java/com/codegym/musicplayer/model/Song.java
@@ -1,4 +1,50 @@
 package com.codegym.musicplayer.model;
 
 public class Song {
+    private String title;
+    private String artist;
+    private String genre;
+    private String link;
+
+    public Song(String title, String artist, String genre, String link) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.link = link;
+    }
+
+    public Song() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
--- src/main/java/com/codegym/musicplayer/model/SongForm.java
+++ src/main/java/com/codegym/musicplayer/model/SongForm.java
@@ -1,4 +1,52 @@
 package com.codegym.musicplayer.model;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class SongForm {
+    private String title;
+    private String artist;
+    private String genre;
+    private MultipartFile link;
+
+    public SongForm(String title, String artist, String genre, MultipartFile link) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.link = link;
+    }
+
+    public SongForm() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public MultipartFile getLink() {
+        return link;
+    }
+
+    public void setLink(MultipartFile link) {
+        this.link = link;
+    }
 }
